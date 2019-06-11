48a5e070742ad4b27e2238b0183eeab576c743bc
zhanglei <zhanglei@papayamobile.cn>
2019-06-11 15:53:41 +0800

    first commit

---
+++ run.py
@@ -0,0 +1,23 @@
+
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic('LeyStudyApp')
+app.config.DB_NAME = 'ley_study'
+app.config.DB_USER = 'zhanglei'
+
+@app.route('/')
+async def test(request):
+    print(request.header)
+    print(request.body)
+    msg = {'message': 'Welcom to 老张学Python'}
+    return json(msg, ensure_ascii=False)
+
+@app.route('/api/login')
+async def test1(request):
+    print(request)
+    msg = {'message': '访问了老张的Login了！'}
+    return json(msg, ensure_ascii=False)
+
+if __name__ == '__main__':
+    app.run(host='192.168.11.195', port=8899)
